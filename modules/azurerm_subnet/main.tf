45126a69fa9637f18afcad0555e55c623458f91c
prashantverma82 <prashantdevopseng@gmail.com>
2026-08-25 16:47:00 +0530

    module code add

--- modules/azurerm_subnet/main.tf
+++ modules/azurerm_subnet/main.tf
@@ -5,5 +5,5 @@ resource "azurerm_subnet" "subnet" {
   name = each.value.name
   resource_group_name = each.value.resource_group_name
  virtual_network_name = each.value.virtual_network_name
- address_prefixes = each.value.address_space 
+ address_prefixes = each.value.address_prefixes
 }
